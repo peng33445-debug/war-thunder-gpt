ba8d0ee2d613a50171b4d42e58bd6417173713c9
peng33445-debug <peng33445@gmail.com>
2026-09-25 15:20:39 +0800

    Refactor Guide to include modules for tank and movement
    Added module structure for tank, movement, and turret with create, update, and render methods.

--- js/core/Guide.js
+++ js/core/Guide.js
@@ -1,11 +1,55 @@
 import { Tank } from "../vehicle/Tank.js";
 import { Movement } from "../movement/Movement.js";
 import { Turret } from "../combat/Turret.js";
+import { inputState } from "../input/Input.js";
 
 const Guide = {
-    Tank,
-    Movement,
-    Turret
+
+    modules: {
+
+        tank: {
+            create(x, y) {
+                return new Tank(x, y);
+            }
+        },
+
+        movement: {
+            create() {
+                return new Movement();
+            },
+
+            update(instance, tank, deltaTime) {
+                instance.update(
+                    tank,
+                    inputState.movement,
+                    deltaTime
+                );
+            }
+        },
+
+        turret: {
+            create() {
+                return new Turret();
+            },
+
+            update(instance, tank, deltaTime) {
+                instance.update(
+                    tank,
+                    inputState.turret,
+                    deltaTime
+                );
+            },
+
+            render(instance, ctx, tank) {
+                instance.draw(
+                    ctx,
+                    tank
+                );
+            }
+        }
+
+    }
+
 };
 
 export { Guide };
