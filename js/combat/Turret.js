e500834c1d5621c0ed6c7bc9c79064cfd7bbc6b4
peng33445-debug <peng33445@gmail.com>
2026-09-25 14:09:21 +0800

    Implement turret update logic with rotation control

--- js/combat/Turret.js
+++ js/combat/Turret.js
@@ -1,1 +1,48 @@
+class Turret {
 
+    update(tank, input, deltaTime) {
+
+        if (!input.active) {
+            return;
+        }
+
+        const targetAngle =
+            Math.atan2(
+                input.y,
+                input.x
+            );
+
+        let angleDifference =
+            targetAngle -
+            tank.turretAngle;
+
+        while (angleDifference > Math.PI) {
+            angleDifference -= Math.PI * 2;
+        }
+
+        while (angleDifference < -Math.PI) {
+            angleDifference += Math.PI * 2;
+        }
+
+        const rotationSpeed =
+            tank.turretTurnSpeed;
+
+        const maxRotation =
+            rotationSpeed * deltaTime;
+
+        if (Math.abs(angleDifference) <= maxRotation) {
+
+            tank.turretAngle =
+                targetAngle;
+
+        } else {
+
+            tank.turretAngle +=
+                Math.sign(angleDifference) *
+                maxRotation;
+
+        }
+    }
+}
+
+export { Turret };
