7831eecab641b3a5809ca86bbb441a190a921e2e
peng33445-debug <peng33445@gmail.com>
2026-09-25 14:52:52 +0800

    Update Tank.js

--- js/vehicle/Tank.js
+++ js/vehicle/Tank.js
@@ -1,12 +1,19 @@
-
 class Tank {
 
     constructor(x, y) {
         this.x = x;
         this.y = y;
 
+        // =========================
+        // 车辆状态
+        // =========================
+
         this.hullAngle = 0;
         this.turretAngle = 0;
+
+        // =========================
+        // V0.1 测试坦克数据
+        // =========================
 
         this.width = 70;
         this.height = 42;
@@ -16,11 +23,11 @@ class Tank {
 
         this.gunLength = 42;
 
-        // V0.1 测试坦克基础数据
         this.turretTurnSpeed = 1.5;
     }
 
     draw(ctx) {
+
         ctx.save();
 
         ctx.translate(
@@ -28,18 +35,13 @@ class Tank {
             this.y
         );
 
-        /*
-         * 车体坐标系
-         *
-         * 炮塔的角度以车体为基准。
-         */
-        ctx.rotate(this.hullAngle);
+        ctx.rotate(
+            this.hullAngle
+        );
 
         // =========================
         // 车体
         // =========================
-
-        ctx.save();
 
         ctx.fillStyle = "#596158";
 
@@ -65,43 +67,6 @@ class Tank {
             this.width,
             6
         );
-
-        ctx.restore();
-
-        // =========================
-        // 炮塔
-        // =========================
-
-        ctx.save();
-
-        /*
-         * turretAngle 是相对于车体的角度。
-         *
-         * 因为这里已经处于 hullAngle
-         * 的坐标系中，所以炮塔会随着
-         * 车体一起转动，同时还能独立旋转。
-         */
-        ctx.rotate(this.turretAngle);
-
-        ctx.fillStyle = "#687067";
-
-        ctx.fillRect(
-            -this.turretWidth / 2,
-            -this.turretHeight / 2,
-            this.turretWidth,
-            this.turretHeight
-        );
-
-        ctx.fillStyle = "#343934";
-
-        ctx.fillRect(
-            0,
-            -4,
-            this.gunLength,
-            8
-        );
-
-        ctx.restore();
 
         ctx.restore();
     }
