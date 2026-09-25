294f8ab9b278500a8577850a249b4a2571bfd8e5
peng33445-debug <peng33445@gmail.com>
2026-09-25 14:15:36 +0800

    Update Turret.js

--- js/combat/Turret.js
+++ js/combat/Turret.js
@@ -6,14 +6,37 @@ class Turret {
             return;
         }
 
-        const targetAngle =
+        /*
+         * 右摇杆方向是世界方向。
+         */
+        const targetWorldAngle =
             Math.atan2(
                 input.y,
                 input.x
             );
 
+        /*
+         * 把世界方向转换成
+         * 相对于坦克车体的方向。
+         */
+        let targetRelativeAngle =
+            targetWorldAngle -
+            tank.hullAngle;
+
+        while (targetRelativeAngle > Math.PI) {
+            targetRelativeAngle -= Math.PI * 2;
+        }
+
+        while (targetRelativeAngle < -Math.PI) {
+            targetRelativeAngle += Math.PI * 2;
+        }
+
+        /*
+         * 炮塔当前角度就是
+         * 相对于车体的角度。
+         */
         let angleDifference =
-            targetAngle -
+            targetRelativeAngle -
             tank.turretAngle;
 
         while (angleDifference > Math.PI) {
@@ -24,6 +47,10 @@ class Turret {
             angleDifference += Math.PI * 2;
         }
 
+        /*
+         * 当前 V0.1 测试坦克的炮塔转速。
+         * 以后这里会从坦克自己的数据读取。
+         */
         const rotationSpeed =
             tank.turretTurnSpeed;
 
@@ -33,14 +60,13 @@ class Turret {
         if (Math.abs(angleDifference) <= maxRotation) {
 
             tank.turretAngle =
-                targetAngle;
+                targetRelativeAngle;
 
         } else {
 
             tank.turretAngle +=
                 Math.sign(angleDifference) *
                 maxRotation;
-
         }
     }
 }
