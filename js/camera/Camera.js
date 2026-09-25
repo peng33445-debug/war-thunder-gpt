f53932a0fd04201ac74bb8dfe74290badaf1bc28
peng33445-debug <peng33445@gmail.com>
2026-09-25 15:30:42 +0800

    Update Camera.js

--- js/camera/Camera.js
+++ js/camera/Camera.js
@@ -1,1 +1,49 @@
+class Camera {
 
+    constructor() {
+
+        this.x = 0;
+        this.y = 0;
+
+        this.width =
+            window.innerWidth;
+
+        this.height =
+            window.innerHeight;
+    }
+
+    resize(width, height) {
+
+        this.width = width;
+        this.height = height;
+    }
+
+    follow(target) {
+
+        this.x =
+            target.x -
+            this.width / 2;
+
+        this.y =
+            target.y -
+            this.height / 2;
+    }
+
+    worldToScreen(x, y) {
+
+        return {
+            x: x - this.x,
+            y: y - this.y
+        };
+    }
+
+    screenToWorld(x, y) {
+
+        return {
+            x: x + this.x,
+            y: y + this.y
+        };
+    }
+}
+
+export { Camera };
