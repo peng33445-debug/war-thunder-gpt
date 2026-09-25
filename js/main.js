1b0495cf93438a45c5a4484b355cd676c901d262
peng33445-debug <peng33445@gmail.com>
2026-09-25 13:53:20 +0800

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,25 +1,10 @@
 import { Game } from "./core/Game.js";
 
-
-/* =========================
-   Canvas
-   ========================= */
-
 const canvas =
     document.getElementById("gameCanvas");
 
-
-/* =========================
-   Game Core
-   ========================= */
-
 const game =
     new Game(canvas);
-
-
-/* =========================
-   试车场按钮
-   ========================= */
 
 const testFieldButton =
     document.getElementById("testFieldButton");
@@ -30,23 +15,13 @@ const mainMenu =
 const gameUI =
     document.getElementById("gameUI");
 
-
 testFieldButton.addEventListener(
     "click",
     () => {
-
         mainMenu.style.display = "none";
-
         gameUI.style.display = "block";
-
         game.start();
-
     }
 );
-
-
-/* =========================
-   启动游戏核心
-   ========================= */
 
 game.run();
